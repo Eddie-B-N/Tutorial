4c9353226cab4be2e37259431cdc843868cf229f
Eddie-B-N <oldies269@gmail.com>
2022-10-11 22:51:41 +0200

    creating models

---
+++ leads/models.py
@@ -0,0 +1,8 @@
+from email.policy import default
+from msilib.schema import Class
+from django.db import models
+
+class Lead(models.Model):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    age = models.IntegerField(default=0)
