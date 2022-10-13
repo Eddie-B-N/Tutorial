90f84a22908ace67fa32b35a5f88309759513c3c
Eddie-B-N <oldies269@gmail.com>
2022-10-13 12:59:42 +0200

    model changes

--- leads/models.py
+++ leads/models.py
@@ -1,8 +1,28 @@
 from email.policy import default
 from msilib.schema import Class
+from tkinter import CASCADE
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    pass
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
 
 class Lead(models.Model):
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
     age = models.IntegerField(default=0)
+    agent = models.ForeignKey("Agent", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+class Agent(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.email
